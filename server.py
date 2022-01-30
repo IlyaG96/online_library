5b69238d61be19278995e90f3d0964ee34384d8b
IlyaG96 <ilyag_96@icloud.com>
2022-01-30 12:33:51 +1100

    Split books list by pages

--- server.py
+++ server.py
@@ -1,30 +1,43 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
-from more_itertools import distribute
+from more_itertools import distribute, chunked
+from pathlib import Path
 
 
-def render_index_page():
+def render_index_pages():
+
+    with open(file='database/books.json', mode='r') as file:
+        books = json.load(file)
+
+    book_pages = list(chunked(books, 10))
 
     env = Environment(
         loader=FileSystemLoader('/Users/ilyagabdrakhmanov/PycharmProjects/online_library/templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    with open(file="database/books.json", mode="r") as file:
-        books = json.load(file)
-    books_col1, books_col2 = distribute(2, books)
 
-    template = env.get_template('template.html')
-    rendered_page = template.render(books_col1=list(books_col1), books_col2=list(books_col2))
+    Path('pages').mkdir(parents=True, exist_ok=True)
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for page_num, page_content in enumerate(book_pages):
+
+        books_col1, books_col2 = distribute(2, page_content)
+
+        template = env.get_template('template.html')
+        rendered_page = template.render(books_col1=list(books_col1), books_col2=list(books_col2))
+        with open(file=f'pages/index{page_num+1}.html', mode='w', encoding="utf8") as file:
+            file.write(rendered_page)
 
     print('site reloaded')
 
 
-render_index_page()
+def main():
 
-server = Server()
-server.watch('templates/template.html', render_index_page)
-server.serve(root='.')
+    render_index_pages()
+    server = Server()
+    server.watch('templates/template.html', render_index_pages)
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
